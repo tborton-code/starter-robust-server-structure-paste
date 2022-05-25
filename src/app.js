bdefcfeb45b0820c226f0ab3372a9d585e585d00
Tyler Borton <tylercborton@gmail.com>
2022-05-25 12:08:07 -0400

    returning one paste

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ const app = express();
 
 const pastes = require("./data/pastes-data");
 
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+
 app.use("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
