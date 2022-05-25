3f146ce4e8f2a186c3f8a8fb048b4e1838c840bf
Tyler Borton <tylercborton@gmail.com>
2022-05-25 14:52:07 -0400

    added app.post

--- src/app.js
+++ src/app.js
@@ -21,6 +21,25 @@ app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
 
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.json({ data: newPaste });
+});
+
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
