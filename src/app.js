6c3b81cc4a3e3e889b385d8820bf6e4eaa7b072a
Tyler Borton <tylercborton@gmail.com>
2022-05-25 12:01:45 -0400

    added route to pastes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
+const pastes = require("./data/pastes-data");
+
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
