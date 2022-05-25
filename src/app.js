b7e6024101197535b6d2f4cc30ef5e8179952514
Tyler Borton <tylercborton@gmail.com>
2022-05-25 16:01:01 -0400

    400 status

--- src/app.js
+++ src/app.js
@@ -26,18 +26,21 @@ app.get("/pastes", (req, res) => {
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
 app.post("/pastes", (req, res, next) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
-  pastes.push(newPaste);
-  res.status(201).json({ data: newPaste });
+  const { data: { name, syntax, exposure, expiration, text } = {} } = req.body;
+if (text) {
+    const newPaste = {
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+} else {
+  res.sendStatus(400);
+}
 });
 
 // Not found handler
