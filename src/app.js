0e0e895a4132adb9e7a70bc1c349e6f50d558b9c
Tyler Borton <tylercborton@gmail.com>
2022-05-26 12:24:29 -0400

    updated calls to next

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${pasteId}`);
+    next({ status: 404, message: `Paste id not found: ${pasteId}` });
   }
 });
 
